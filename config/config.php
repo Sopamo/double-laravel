fccf51557120e2350c70ecf2491b867cc1da1c35
Paul Mohr <p.mohr@sopamo.de>
2022-04-07 18:41:51 +0200

    Update default api path

--- config/config.php
+++ config/config.php
@@ -3,7 +3,7 @@
 return [
     // The prefix that's used for all `double` API routes
     // You can override this in case you already have routes starting with `double`
-    'api_prefix' => 'double',
+    'api_prefix' => 'api/double',
 
     // The root of your vuejs frontend. Files in this folder have to be executable by php
     // By default this is next to your Laravel's `src` folder and called `double`
