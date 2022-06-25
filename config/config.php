32f042bc68e34704dd91dc8e7ece073748729ca7
Paul Mohr <p.mohr@sopamo.de>
2022-06-25 09:42:24 +0200

    update comments

--- config/config.php
+++ config/config.php
@@ -2,10 +2,10 @@
 
 return [
     // The prefix that's used for all `double` API routes
-    // You can override this in case you already have routes starting with `double`
+    // You can override this in case you already have routes starting with `api/double`
     'api_prefix' => 'api/double',
 
-    // The root of your vuejs frontend. Files in this folder have to be executable by php
-    // By default this is next to your Laravel's `src` folder and called `double`
+    // The root of your Vue.js frontend. Files in this folder have to be executable by php.
+    // By default, this points to a folder called `double` next to your Laravel's `src` folder
     'frontend_root' => base_path('double'),
 ];
