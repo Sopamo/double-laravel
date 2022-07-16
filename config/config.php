22269da6ba03cc06bf2fe7285c10957b14748c47
Paul Mohr <p.mohr@sopamo.de>
2022-07-16 11:50:32 +0200

    Fix default double-vue path

--- config/config.php
+++ config/config.php
@@ -7,5 +7,5 @@ return [
 
     // The root of your Vue.js frontend. Files in this folder have to be executable by php.
     // By default, this points to a folder called `double` next to your Laravel's `src` folder
-    'frontend_root' => base_path('double'),
+    'frontend_root' => base_path('double/src'),
 ];
